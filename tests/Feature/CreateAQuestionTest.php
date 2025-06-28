7d9b065bae3917e843ada1a7e718500319735290
Walber Vaz <wvs.walber@gmail.com>
2025-06-28 17:17:05 -0300

    Feature/create new question (#20)
    * test(question): should be able to create a new question bigger than 255 characters
    
    * test(question): should have at least 10 characters in the question
    
    * test(question): should have a question ending with a question mark

---
+++ tests/Feature/CreateAQuestionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types = 1);
+
+use App\Models\User;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\post;
+
+it('should be able to create a new question bigger than 255 characters', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $request = post(route('question.store'), [
+        'question' => str_repeat('a', 260) . '?',
+    ]);
+
+    $request->assertRedirect(route('dashboard'));
+    assertDatabaseCount('questions', 1);
+    assertDatabaseHas('questions', ['question' => str_repeat('a', 260) . '?']);
+});
+
+it('should have a question ending with a question mark', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $request = post(route('question.store'), [
+        'question' => 'This is a question',
+    ]);
+
+    $request->assertSessionHasErrors(['question' => __('validation.ends_with', ['attribute' => 'question', 'values' => '?'])]);
+    assertDatabaseCount('questions', 0);
+});
+
+it('should have at least 10 characters in the question', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $request = post(route('question.store'), [
+        'question' => str_repeat('a', 8) . '?',
+    ]);
+
+    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+    assertDatabaseCount('questions', 0);
+});
