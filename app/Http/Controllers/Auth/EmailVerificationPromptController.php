84bb986949fab179f4ace0959d3ad2c1943389d8
Walber Vaz <wvs.walber@gmail.com>
2025-06-28 13:16:08 -0300

    Feature/key 2 setup develop (#16)
    * chore(set): setup laravel pint
    
    * chore(set): setup larastan
    
    * fix(config): config pre-commit hook local
    
    * chore(config): config hook local test
    
    * fix(config): config commit conventional

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
@@ -12,7 +14,7 @@ class EmailVerificationPromptController extends Controller
     /**
      * Display the email verification prompt.
      */
-    public function __invoke(Request $request): RedirectResponse|View
+    public function __invoke(Request $request): RedirectResponse | View
     {
         return $request->user()->hasVerifiedEmail()
                     ? redirect()->intended(route('dashboard', absolute: false))
