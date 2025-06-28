779bfd1acb945b824dc68e8bcddce52ad28b2dab
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-06-28 20:42:43 -0300

    feat(question): add Question model and factory, update configuration for localization

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Question>
+ */
+class QuestionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'question' => $this->faker->realText(50),
+        ];
+    }
+}
