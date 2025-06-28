e78365b4a774b21514cd2de83ba1d34397f70f5d
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-06-28 20:44:02 -0300

    fix(factory): update strict types declaration and improve docblock annotation

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -4,10 +4,11 @@ declare(strict_types = 1);
 
 namespace Database\Factories;
 
+use App\Models\Question;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Question>
+ * @extends Factory<Question>
  */
 class QuestionFactory extends Factory
 {
